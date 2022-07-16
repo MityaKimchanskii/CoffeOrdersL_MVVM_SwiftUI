6952010debf5f056cca4e0385ff6dfaf11b2338a
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-15 22:20:21 -0700

    Initial Commit

---
+++ CoffeOrdersL_MVVM_SwiftUI/CoffeOrdersL_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeOrdersL_MVVM_SwiftUIApp.swift
+//  CoffeOrdersL_MVVM_SwiftUI
+//
+//  Created by Mitya Kim on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeOrdersL_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
